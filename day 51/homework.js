78a0151491ffb23e47399e0c3195666bba4057e2
ludovikuna12 <lpicxalaishvili13@gmail.com>
2024-03-15 21:29:02 +0400

    commit

---
+++ day 51/homework.js
@@ -0,0 +1,21 @@
+alert("hi ther")
+console.log(10*10)
+console.log(12/4)
+console.log(100/4)
+console.log(19+20)
+console.log(110*22)
+console.log(200-20)
+console.log(999-100)
+console.log(10000000-1)
+console.log(2+2)
+
+
+
+
+
+
+
+
+
+
+
