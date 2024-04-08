c00c62536760ea8ce670ebcb668d37fb45d17f73
ludovikuna12 <lpicxalaishvili13@gmail.com>
2024-04-08 19:25:04 +0400

    commit

---
+++ day 58/homework.js
@@ -0,0 +1,63 @@
+/*let name = "luka"
+
+let lastName = "pircxalaishvili"
+
+let age = 14
+
+
+console.log("hi my name is" +" "+ name +" "+ "and my last Name is" +" "+ lastName +" "+ "and my age is" +" "+ age);*/
+
+/*let no = false
+
+let yas = true
+
+console.log("did you do your homework" +" "+ yas);
+
+console.log("did you not do your homework" + " " + no);*/
+
+/*let number = 4
+
+let number2 =9
+
+let number3 = 14
+
+let number4 = 19
+
+console.log(++number);
+
+console.log(++number2);
+
+console.log(++number3);
+
+console.log(++number4);
+
+console.log(--number);
+
+console.log(--number2);
+
+console.log(--number3);
+
+console.log(--number4);*/
+
+/*alert("hay ther");*/
+
+/*let number = 23
+number = 40
+
+let number2 = 65
+number2 = 80
+
+let number3 = 90
+number3 = 100
+
+let number4 = 139
+number4 = 150
+
+console.log(number + 65 );
+console.log(number2 + 90);
+console.log(number3 + 120);
+console.log(number4 + 150);
+console.log(number - 45 );
+console.log(number2 - 50);
+console.log(number3 - 80);
+console.log(number4 - 100);*/
