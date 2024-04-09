5c8aec2c0c560fec778a8fe2fb2cdf8307f37a0b
ludovikuna12 <lpicxalaishvili13@gmail.com>
2024-04-10 00:01:15 +0400

    commit

---
+++ day 59/homework.js
@@ -0,0 +1,48 @@
+/*function math(x,y,z) {
+
+console.log(x*z);
+console.log(y/z);
+console.log(x+y);
+console.log(z-x);
+
+}
+*
+
+
+
+/*function rectangl(katedi,katedi2,hipotenuza) {
+console.log(2*(katedi+katedi2+hipotenuza));
+}
+*/
+
+
+
+/*function GOA() {
+    console.log("GOA best");
+    console.log("GOA best");
+    console.log("GOA best");
+    console.log("GOA best");
+    console.log("GOA best");
+    console.log("GOA best");
+    console.log("GOA best");
+    console.log("GOA best");
+    console.log("GOA best");
+    console.log("GOA best");
+}
+*/
+
+/*function martkutxedi(x,y) {
+console.log(2*(x+y))
+}
+*/
+
+
+
+
+function me(name,lastName,age,adgras) {
+console.log("my name is "+ name + " and my last name is " + lastName + "and a am  " + age + " yers old and i live in " + adgras);
+}
+
+
+
+
