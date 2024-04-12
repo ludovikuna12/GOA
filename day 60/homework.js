7079c6c4d478dbb49246e851fc05d985c62adcd5
ludovikuna12 <lpicxalaishvili13@gmail.com>
2024-04-13 01:55:38 +0400

    commit

---
+++ day 60/homework.js
@@ -0,0 +1,93 @@
+//შემოატანინეთ მომხამერებელს მისი მთელი ოჯახის სახელი გვარი და საცხოვრებელი ადგილი შემდეგ კი გამოიტანე ეს ყველაფერი ერთ წინადადებაში
+
+//1 კითხვა
+
+
+//let me = prompt("enter ur name")
+//let lastName = prompt("enter ur last name")
+//let age = prompt("enter ur age")
+//let liveIn = prompt("where do you live")
+
+//alert(`my name is ${me}`)
+//alert(`my lastName is ${lastName}`)
+//alert(`my age is ${age}`)
+//alert(`and i live in ${liveIn}`)
+
+
+
+//let mam = prompt("enter ur name")
+//let lastName = prompt("enter ur last name")
+//let age = prompt("enter ur age")
+//let liveIn = prompt("where do you live")
+
+//alert(`mams name is ${mam}`)
+//alert(`mams lastName is ${lastName}`)
+//alert(`mams age is ${age}`)
+//alert(`and mam live in ${liveIn}`)
+
+
+
+//let dad = prompt("enter ur name")
+//let lastName = prompt("enter ur last name")
+//let age = prompt("enter ur age")
+//let liveIn = prompt("where do you live")
+
+//alert(`dads name is ${dad}`)
+//alert(`dads lastName is ${lastName}`)
+//alert(`dads age is ${age}`)
+//alert(`and dad live in ${liveIn}`)
+
+//დავალება 2
+
+//შემოატანინეთ მომხმარებელს მისი ოჯახის თითოეული წევრის ასაკი შემდეგ კი შემოატანინე თუ რამდენი ხნით სურთ  დროში მოგზაურობა და საბლოოდ გაოიტანე თუ რამდენი წლის იქნება თითოეული ოჯახის წევრი დროში მოგზაურობის შემდეგ
+
+//let me = prompt("what is your age")
+//alert(`my age is ${me}`)
+
+//let mam = prompt("what is your mams age")
+//alert(`mams age is ${mam}`)
+
+//let dad = prompt("what is your dads age")
+//alert(`dads age is ${dad}`)
+
+//console.log(me + mam);
+//console.log(me + dad);
+//console.log(dad + mam);
+
+
+//დავალება 4
+
+//ააწყვეთ საიტი სადაც ჩართავთ prompt ფუნქციას
+
+//let me = prompt("enter ur name")
+//let lastName = prompt("enter ur last name")
+//let age = prompt("your age")
+//let phoneNumber = prompt("your phone number")
+
+//console.log(`your name is ${me}`)
+//console.log(`your lastName is ${lastName}`)
+//console.log(`your age is ${age}`)
+//console.log(`your phoneNumber is ${phoneNumber}`)
+
+
+
+let anything = ("bad")
+let anybody = ("the roke")
+let anyflawer = ("Rose")
+let anywere = ("up")
+let number = 34
+let number2 = 87
+let number3 = 12
+let number4 = 65
+
+console.log(typeof(anything))
+console.log(typeof(anybody))
+console.log(typeof(anyflawer))
+console.log(typeof(anywere))
+console.log(typeof(number))
+console.log(typeof(number2))
+console.log(typeof(number3))
+console.log(typeof(number4))
+
+
+
