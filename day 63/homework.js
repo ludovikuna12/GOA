9c2406cc0791d4aab4615e83fc2698ed6867cdac
ludovikuna12 <lpicxalaishvili13@gmail.com>
2024-04-26 19:46:23 +0400

    commit

---
+++ day 63/homework.js
@@ -0,0 +1,39 @@
+/*let user  = {
+    name:"luka",
+    age:14,
+    adras:"gldani"
+}
+
+console.log(user)*/
+
+
+
+
+
+
+/*let myFamly = {
+    mamAge:31,
+    dadAge:34,
+    grandfothersAge:60,
+    grandmother:46
+}
+
+console.log(myFamly.mamAge+5);
+console.log(myFamly.dadAge+5);
+console.log(myFamly.grandfothersAge+5);
+console.log(myFamly.grandmother+5);*/
+
+
+
+
+
+/*let user  = {
+    name:prompt("what is your name"),
+    age:Number(prompt("what is your age")),
+    adras:prompt("what is your adras")
+}
+
+console.log(user.name)
+console.log(user.age)
+console.log(user.adras)
+*/
