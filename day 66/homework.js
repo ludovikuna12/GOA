065843f5ae39c77da692dc61c2d3456aa3328c30
ludovikuna12 <lpicxalaishvili13@gmail.com>
2024-05-11 19:21:23 +0400

    commit

---
+++ day 66/homework.js
@@ -0,0 +1,14 @@
+let passwerd = prompt("enter your password")
+
+let Email = prompt("enter your email")
+
+let repit1 = prompt("enter your password agan")
+
+let repit2 = prompt("enter your email agan")
+
+
+if(passwerd === repit1 || Email === repit2){
+    alert("true")
+} else {
+    alert("fals")
+}
