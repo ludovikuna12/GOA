3add053bc3e044beba448e7e42fcf1b10b4c47f7
ludovikuna12 <lpicxalaishvili13@gmail.com>
2024-05-16 20:16:36 +0400

    commit

---
+++ day 70/homework.js
@@ -0,0 +1,17 @@
+//1 დავალება
+//ააგეთ საიტი ნებისმიერ თემატიკასთან დაკავშირებით და შემდეგ ამ საიტზე არსებული დიზაინი შეცვალეთ JS ფაილიდან(DOM გამოყენებით)
+
+let img = document.getElementById("imgofgoa")
+
+let Goa = document.getElementById("whatisgoa")
+
+let clas = document.getElementById("classis")
+
+
+img.style.height = "300px"
+
+Goa.style.color = "green"
+
+clas.style.textShadow = "4px 4px 4px green"
+
+clas.style.fontSize = "30px"
---
+++ day 70/homework2.js
@@ -0,0 +1,10 @@
+//2 დავალება
+//წამოიღეთ ელემენტები HTML ფაილიდან JS ფაილში სხვადასხვა მეთოდის გამოყენებით. (მინიშნება - document.getElementById())
+
+let h1 = (document.getElementById("iorem"))
+
+let p = (document.getElementsByClassName("lorem2"))
+
+let b = (document.getElementsByName("b"))
+
+console.log(h1,p,b)
