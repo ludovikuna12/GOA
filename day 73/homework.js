c89f5e1a132bef41cb11315e437cc486d562db6e
ludovikuna12 <lpicxalaishvili13@gmail.com>
2024-05-26 20:21:01 +0400

    commit

---
+++ day 73/homework.js
@@ -0,0 +1,11 @@
+function  book (title,author,year) {
+this.title = title
+this.author = author
+this.year = year
+}
+
+let thebook1 = new book ("The Great Gatsby","F. Scott Fitzgerald",1925)
+let thebook2 = new book ("ვეფხისტყაოსანი","შოთარუსთაველი",2021)
+
+console.log(thebook1)
+console.log(thebook2)
