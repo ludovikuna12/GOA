71014a00ba37bd03fc594b293cd21047ae738517
ludovikuna12 <lpicxalaishvili13@gmail.com>
2024-05-30 21:08:10 +0400

    commit

---
+++ day 74/homework.js
@@ -0,0 +1,43 @@
+//let name = prompt("enter your name")
+
+//let lestname = prompt("enter your lestname")
+
+//alert("Welcome luka")
+
+//alert("Welcome pircxalaishvili")
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+//function xandy () {
+//    for (let x = 1 ; x < 10 ; x ){
+//        console.log(x * 2)
+//   }
+//}
+//xandy ()
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+
+//function xandy (x) {
+//    for(let x = 0 ; x = 5 ; x++) {
+//        console.log(x);
+//    }
+//}
+
+//xandy(0)
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+
