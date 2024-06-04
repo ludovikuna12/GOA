de82f831514785fa7e58f44d11c097db602d1d5a
ludovikuna12 <lpicxalaishvili13@gmail.com>
2024-06-04 22:07:45 +0400

    commit

---
+++ day75/homework.js
@@ -0,0 +1,38 @@
+/*let data  = ["oil",42,true,false,3.2,"suger",3,10,"fluffy cake",2.4 ]
+
+data[1] = "flower"
+data[6] = 34
+data[9] = "not so fluffy cake"
+data[10] = 3
+data[4] = true
+
+
+*/
+
+
+//2) ბევრი იმეცადინეთ, ბევრი კოდი წერეთ და იექსპერიმენტეთ
+/*
+
+let data1 = ["luka",14,true,4.3]
+
+console.log(typeof(data1))
+
+*/
+/////////////////////////////////////////////////////////////////////////////////////////
+/*
+
+function dat(data3) {
+    let data4 = ["luka","pircxalaishvili",14,5.6]
+    console.log(data3)
+}
+dat()
+*/
+////////////////////////////////////////////////////////////////////////////////////////////
+/*
+
+
+let data5 = ["year",2010,"name","luka","laetname","pircxalaishvili","age",14]
+alert(data5)
+
+
+*/
